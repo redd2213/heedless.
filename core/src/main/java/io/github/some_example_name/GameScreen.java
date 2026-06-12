4febc3a14f1c219fc5b9a705b202e1524a4e9a3a
redd2213 <anghel.radu000@gmail.com>
2026-06-12 11:16:37 +0300

    feat: Added player, WASD input, camera follow and screen clamping.

--- core/src/main/java/io/github/some_example_name/GameScreen.java
+++ core/src/main/java/io/github/some_example_name/GameScreen.java
@@ -1,17 +1,70 @@
 package io.github.some_example_name;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 /** First screen of the application. Displayed after the application is created. */
 public class GameScreen implements Screen {
+    private SpriteBatch batch;
+    private Texture playerTexture;
+    private float playerX = 100;
+    private float playerY = 100;
+    private float speed = 200; // pixels per second
+    private OrthographicCamera camera;
     @Override
     public void show() {
         // Prepare your screen here.
+        batch = new SpriteBatch();
+        Pixmap pixmap = new Pixmap(32, 32, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.RED);
+        pixmap.fill();
+        playerTexture = new Texture(pixmap);
+        pixmap.dispose();
+        Gdx.input.setInputProcessor(null);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            playerX += speed * delta;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            playerX -= speed * delta;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            playerY += speed * delta;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            playerY -= speed * delta;
+        }
+
+        playerX = MathUtils.clamp(playerX, 0, Gdx.graphics.getWidth() - playerTexture.getWidth());
+        playerY = MathUtils.clamp(playerY, 0, Gdx.graphics.getHeight() - playerTexture.getHeight());
+
+        ScreenUtils.clear(Color.BLACK);
+
+        camera.position.set(playerX, playerY, 0);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(playerTexture, playerX, playerY);
+        batch.end();
+
     }
 
     @Override
